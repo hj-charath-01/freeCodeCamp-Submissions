99fd9f2270765d3e1e71eba3153f20e286a16512
Charath <hj_charath_01@proton.me>
2026-09-21 18:11:37 +0530

    Completed a JavaScript Lab

---
+++ JavaScript Submissions/Lab/2) Build a Sentence Maker.js
@@ -0,0 +1,19 @@
+let adjective = "amazing"
+let noun = "dragon"
+let verb = "scared"
+let place = "cave"
+let adjective2 = "huge"
+let noun2 = "mushrooms"
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a "+ place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+console.log("First story: " + firstStory)
+
+adjective = "huge"
+noun = "mouse"
+verb = "hungry"
+place = "hole"
+adjective2 = "firey"
+noun2 = "chese"
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a "+ place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+console.log("Second story: " + secondStory)
