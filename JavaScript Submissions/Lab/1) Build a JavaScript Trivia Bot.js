5a77ec0364f841f178f08feaecf9df6b4aee80b6
Charath <hj_charath_01@proton.me>
2026-09-21 15:56:18 +0530

    Completed a JavaScript Lab

---
+++ JavaScript Submissions/Lab/1) Build a JavaScript Trivia Bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Fun Fact Bot";
+let botLocation = "Earth";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage;
+console.log(codingFact);
+
+codingFact += "favoriteLanguage";
+console.log(codingFact);
+
+codingFact += "favoriteLanguage";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
